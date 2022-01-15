b4efbe021ef8e6b58a2a95da43d999804e764196
LFernandoPortugal <falconbladex1@gmail.com>
2022-01-15 09:50:56 -0500

    a

--- js/scripts.js
+++ js/scripts.js
@@ -5,5 +5,5 @@ document.getElementById("currentyear").innerHTML = year1;
 
 // console.log(yearm)
 
-let latestDate = new Date(document.lastModified);
+let latestDate = document.lastModified
 document.getElementById("lastmodified").innerHTML = latestDate;
