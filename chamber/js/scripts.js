ac679fdb44b3e1a45c86c0dfcef9a06d6757b9f6
LFernandoPortugal <falconbladex1@gmail.com>
2022-01-30 01:00:33 -0500

    ok

--- chamber/js/scripts.js
+++ chamber/js/scripts.js
@@ -7,3 +7,27 @@ document.getElementById("currentyear").innerHTML = year1;
 
 let latestDate = document.lastModified
 document.getElementById("lastmodified").innerHTML = latestDate;
+
+function toggleMenu() {
+    document.getElementById("nav").classList.toggle("open");
+}
+
+const y = document.getElementById("hamburgerBtn")
+y.onclick = toggleMenu;
+
+// select the elements to manipulate (output to)
+const datefield = document.querySelector("date");
+const datefieldUK = document.querySelector("aside"); // for european/family history format with day first.
+
+// derive the current date using a date object
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+	now
+);
+const fulldateUK = new Intl.DateTimeFormat("en-UK", {
+	dateStyle: "full"
+}).format(now);
+// long, medium, short options ... try them
+
+datefield.innerHTML = `<em>${fulldate}</em>`;
+datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
