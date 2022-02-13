3279b26fd93f075f02ca66fbb659146ea725475d
LFernandoPortugal <falconbladex1@gmail.com>
2022-02-12 23:59:50 -0500

    ok

--- chamber/js/windchill.js
+++ chamber/js/windchill.js
@@ -1,20 +1,32 @@
 // weather script
 const t = parseFloat(document.querySelector("#temp").textContent);
 const s = parseFloat(document.querySelector("#speed").textContent);
-let windchill = "";
 
-if (t <= 50 && s > 3) {
-  windchill = windChill(t, s);
-  windchill = `${windchill}&#176;F`;
-} else {
-  windchill = "N/A";
+
+if(t <= 50 && s > 3) {
+    const windchill = 35.74 + 0.6215 * t - 35.75 * Math.pow(s,0.16) + 0.4275 * t * Math.pow(s,0.16)
+    document.querySelector("#chill").innerHTML = Math.round(windchill) + " ";
+}
+else {
+    document.querySelector("#chill").innerHTML = "N/A"
 }
 
-document.querySelector("#chill").innerHTML = windchill;
 
-function windChill(temp, speed) {
-  let windChill = 35.74 + 0.6215 * temp - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed, 0.16);
-  return windChill.toFixed(2);
-}
+// ANOTHER WAY
+// let windchill = "";
 
-console.log(windchill)
+// if (t <= 50 && s > 3) {
+//   windchill = windChill(t, s);
+//   windchill = windchill+" " ;
+// } else {
+//   windchill = "N/A";
+// }
+
+// document.querySelector("#chill").innerHTML = windchill;
+
+// function windChill(temp, speed) {
+//   let windChill = 35.74 + 0.6215 * temp - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed, 0.16);
+//   return windChill.toFixed(2);
+// }
+
+// console.log(windchill)
