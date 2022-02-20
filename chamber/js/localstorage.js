ad04cb477960f24b7d71eaabd29a9ed08dd3bfa6
LFernandoPortugal <falconbladex1@gmail.com>
2022-02-19 23:52:49 -0500

    ok

--- chamber/js/localstorage.js
+++ chamber/js/localstorage.js
@@ -4,6 +4,9 @@ let lastVisitText = document.querySelector('#days');
 let lastVisitDays = window.localStorage.getItem("lastVisit");
 
 const factor = 1000 * 60 * 60 * 24;
+let daysBetween = Date.now() - lastVisitDays;
+let numberOfDays = daysBetween / factor;
+
 
 if (lastVisitDays == null) {
     lastVisitText.innerHTML = "Welcome to your first visit!"// new Date(Date.now());
@@ -12,8 +15,7 @@ if (lastVisitDays == null) {
 }
 
 
-let daysBetween = Date.now() - lastVisitDays;
-let numberOfDays = daysBetween / factor;
+
 
 window.localStorage.setItem('lastVisit', new Date(Date.now()));
 
