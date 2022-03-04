7b76d6527945c146f45b812aa3bfde15f3b59cc0
LFernandoPortugal <falconbladex1@gmail.com>
2022-03-04 01:38:42 -0500

    ok

--- lesson-9/js/prophets.js
+++ lesson-9/js/prophets.js
@@ -18,25 +18,39 @@ function displayProphets(prophet) {
   let portrait = document.createElement('img');
   let birthdate = document.createElement('h3');
   let birthplace = document.createElement('h3');
+  let order = ``;
+
+  switch (prophet.order) {
+    case 1:
+      order = `${prophet.order}st`;
+      break;
+    case 2:
+      order = `${prophet.order}nd`;
+      break;
+    case 3:
+      order = `${prophet.order}rd`;
+      break;
+    default:
+      order = `${prophet.order}th`;
+  }
   
 
   // Change the textContent property of the h2 element to contain the prophet's full name
-  h2.textContent = `${prophet.name} ${prophet.lastname}`;
+  h2.innerHTML = `${prophet.name} <span class="highlight">${prophet.lastname}</span>`;
   birthdate.textContent = `Date of Birth: ${prophet.birthdate}`;
   birthplace.textContent = `Place of Birth: ${prophet.birthplace}`;
 
   // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values.
   portrait.setAttribute('src', prophet.imageurl);
-  portrait.setAttribute('alt', `Portait of ${prophet.name} ${prophet.lastname}`);
+  portrait.setAttribute('alt', `Portait of ${prophet.name} ${prophet.lastname} - ${order} Latter-Day President`);
   portrait.setAttribute('loading', 'lazy');
 
   // Add/append the section(card) with the h2 element
   card.appendChild(h2);
-  card.appendChild(portrait);
   card.appendChild(birthdate);
   card.appendChild(birthplace);
-  
+  card.appendChild(portrait);
 
   // Add/append the existing HTML div with the cards class with the section(card)
-  document.querySelector('div.cards').appendChild(card);
+  cards.appendChild(card);
 }
