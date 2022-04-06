41b3bdff9bccaeaa3d47df20ae3d4c863331a909
LFernandoPortugal <falconbladex1@gmail.com>
2022-04-06 01:42:45 -0500

    ok

--- finalproject/js/scripts.js
+++ finalproject/js/scripts.js
@@ -1,23 +1,23 @@
-/*=============== VARIABLES MENU ===============*/
+// =============== VARIABLES MENU ===============
 const navMenu = document.getElementById('nav-menu'),
       navToggle = document.getElementById('nav-toggle'),
       navClose = document.getElementById('nav-close')
 
-/*===== MENU SHOW =====*/
+// ===== MENU SHOW =====
 if(navToggle){
     navToggle.addEventListener('click', () =>{
         navMenu.classList.add('show-menu')
     })
 }
 
-/*===== MENU HIDDEN =====*/
+// ===== MENU HIDDEN =====
 if(navClose){
     navClose.addEventListener('click', () =>{
         navMenu.classList.remove('show-menu')
     })
 }
 
-/*=============== REMOVE MENU MOBILE ===============*/
+// =============== REMOVE MENU MOBILE ===============
 const navLink = document.querySelectorAll('.nav_link')
 
 function linkAction(){
@@ -25,3 +25,7 @@ function linkAction(){
     navMenu.classList.remove('show-menu')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
+
+// ================ LAST MODIFIED ==================
+let latestDate = document.lastModified
+document.getElementById("lastmodified").innerHTML = latestDate;
